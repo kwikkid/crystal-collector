9931ca05961563d6d511e7359776e34a161e5b44
lynne.kwik@gmail.com <lynne.kwik@gmail.com>
2019-04-05 20:02:46 -0700

    bug fixes and front end edits

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # crystal-collector
+The web browser generates a random number
+At the start of the game, you won't know the value of the gems until you click on them.
+As you click on the gems, the values of those gems get added together under "Your Score"
+Try to click the correct gems so that your score is the same as the randomly generated number.
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,24 +6,101 @@ var gem1 = 0;
 var gem2 = 0;
 var gem3 = 0;
 var gem4 = 0;
-var isWinner = false;
 var wins = 0;
 var losses = 0;
 
 
 //use a function to start the game
 	function startGame() {
-	
-		isWinner = false;
+		reset();
+		// result(randomNumber);// console.log(typeof gem1);
+		$("#gem1").click(function() {
+			count = gem1 + count
+			$("#count").text(count);
+			console.log("new count ", count );
+			checkWin(randomNumber, count);
+		});
+		$("#gem2").click(function() {
+			count = gem2 + count
+			$("#count").text(count);
+			checkWin(randomNumber, count);
+		});
+		$("#gem3").click(function() {
+			count = gem3 + count
+			$("#count").text(count);
+			checkWin(randomNumber, count);
+		});
+		$("#gem4").click(function() {
+			count = gem4 + count
+			$("#count").text(count);
+			checkWin(randomNumber, count);
 
-		// $("#gem1", "#gem2", "#gem3", "#gem4", "#randon-number", "#count").empty(); 
+
+
+		});	
+		
+
+		//If user clicks Start Over, execute startGame function//
+
+	};
+
+	startGame();
+
+
+	
+	function result(randomNumber) {
+		$("#gem1").click(function() {
+			count = gem1 + count
+			$("#count").text(count);
+			console.log("new count ", count );
+			checkWin(randomNumber, count);
+		});
+		$("#gem2").click(function() {
+			count = gem2 + count
+			$("#count").text(count);
+			checkWin(randomNumber, count);
+		});
+		$("#gem3").click(function() {
+			count = gem3 + count
+			$("#count").text(count);
+			checkWin(randomNumber, count);
+		});
+		$("#gem4").click(function() {
+			count = gem4 + count
+			$("#count").text(count);
+			checkWin(randomNumber, count);
+
+		});	
+	
+
+	};
+
+	function checkWin(randomNumber, count) {
+		console.log("random number, count:" ,randomNumber, count);
+		if(count > randomNumber) {
+			alert("sorry, you lost!"); 
+			losses++
+			$("#losses").html(losses)
+			console.log("losses:", losses)
+			reset();
+		}
+		else if(count == randomNumber) {
+			alert("congrats, you win!")
+			wins++;
+			$("#wins").html(wins)
+			reset()
+		}
+
+	};
+
+	$("#startover").click(reset);
+
+	function reset() {
 		$("#random-number").empty();
 		$("#count").empty();
 		count = 0;
 		console.log("count: ", count);
-		console.log("empty div: ", )
 		//creates a random number generator that displays the number in id random-number between 19-120//
-		// randomNumber = Math.floor(Math.random() * 102 + 19);
 		randomNumber = Math.floor(Math.random() * 102 + 19);
 		console.log("random number: " ,randomNumber);
 		$("#random-number").text(randomNumber);
@@ -34,63 +111,8 @@ var losses = 0;
 		gem3 = Math.floor(Math.random() * 11 + 1);
 		gem4 = Math.floor(Math.random() * 11 + 1);
 		console.log(gem1,gem2,gem3,gem4);
-		console.log(typeof gem1);
-		result(randomNumber);
-	};
-	startGame();
 
-	
-	function result(randomNumber) {
-		$("#gem1").click(function() {
-			count = gem1 + count
-			$("#count").text(count);
-			console.log("first");
-			console.log(count);
-			checkWin(randomNumber, count);
-		});
-		$("#gem2").click(function() {
-			count = gem2 + count
-			$("#count").text(count);
-			console.log("first");
-			checkWin(randomNumber, count);
-		});
-		$("#gem3").click(function() {
-			count = gem3 + count
-			$("#count").text(count);
-			console.log("first");
-			checkWin(randomNumber, count);
-		});
-		$("#gem4").click(function() {
-			count = gem4 + count
-			$("#count").text(count);
-			console.log("first");
-			checkWin(randomNumber, count);
-
-		});	
-		
-
-	};
-
-	function checkWin(randomNumber, count) {
-		console.log("inside checkWin");
-		console.log("random number, count:" ,randomNumber, count);
-		if(count>randomNumber){
-			console.log("second")
-			alert("sorry");
-			losses++;
-			$("#losses").html(losses);
-			console.log("losses:", losses)
-		startGame();
-		}
-		else if(count == randomNumber){
-			console.log("second");
-			alert("yaya");
-			wins++;
-			$("#wins").html(wins);
-		startGame();
-			
-		}
-	};
+	}
 
 
 });
